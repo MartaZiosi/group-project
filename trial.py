336067e301f68194e003596719be7cd5f5de67f5
Hiranmayee Rajan <H.Rajan@lse.ac.uk>
2018-03-02 20:17:05 +0000

    Added a page to show all registered complaints from the database in table format

--- trial.py
+++ trial.py
@@ -1,13 +1,4 @@
-# from flask import Flask, render_template, request
-#
-# app = Flask("MyApp")
-#
-# @app.route("/")
-# def hello():
-#     return "Hello world!"
-#
-# if __name__ == "__main__":
-#     app.run(debug = True)
+
 from flask import Flask, render_template, request
 import requests
 import psycopg2
@@ -24,8 +15,7 @@ def complaints_form():
 #try defining the same route twice and associated with different functions. It's not good practice to usually do this though
 def thank_you():
     form_data = request.form
-    main()
-    #main(form_data["flat"], form_data["name"], form_data["comment"])
+    add_data()
     send_simple_message(form_data["flat"], form_data["name"], form_data["comment"], form_data["email"]) #the order of definition doesn't matter. This is because, here, I'm not calling the function send_simple_message until I go to /thankyou
     return render_template("thankyou.html", data=form_data)
 
@@ -38,29 +28,24 @@ def send_simple_message(flat, name, complaint, email):
               "subject": "Your Complaint Summary",
               "text": "Hi {}, we've received the complaint {} for flat {}".format(name, complaint, flat)})
 
-
-def main():
+def add_data():
     form_data = request.form
-    # na=form_data["name"]
-    # fl=form_data["flat"]
-    # com=form_data["comment"]
-    #time = datetime.now()
     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
     cursor.execute("INSERT INTO complaints (flat, name, comment, created, resolved) VALUES ('{}','{}', '{}', '{}', '{}')".format(form_data["flat"], form_data["name"], form_data["comment"], str(datetime.now()), False))
     conn.commit()
-
-    #cursor.close()
     conn.close()
 
-    #
-#     records = cursor.fetchall()
-#     #print(records)
-#     for number in range(len(records[0])):
-#         print(records[0][number])
-#
-# if __name__ == "__main__":
-#     main()
+
+@app.route("/complaints", methods=["GET"])
+def show_complaints():
+    conn_string = "host = 'localhost' dbname = 'tenants' user='postgres' password='password'"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM complaints")
+    records = cursor.fetchall()
+    return render_template("complaints.html", records_list = records)
+
 
 app.run(debug = True)
