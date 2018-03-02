a7bad8919f1f0551031bd671ea2100edcf901b47
Hiranmayee Rajan <H.Rajan@lse.ac.uk>
2018-03-02 03:35:48 +0000

    sensitive info

--- trial.py
+++ trial.py
@@ -64,22 +64,3 @@ def main():
 #     main()
 
 app.run(debug = True)
-<<<<<<< HEAD
-=======
-
-#
-# import psycopg2
-#
-# def main():
-#     conn_string = "host = 'localhost' dbname = 'tenants' user='postgres' password='***REMOVED***'"
-#     conn = psycopg2.connect(conn_string)
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT flat, name, comment, created, resolved FROM complaints")
-#     records = cursor.fetchall()
-#     #print(records)
-#     for number in range(len(records[0])):
-#         print(records[0][number])
-#
-# if __name__ == "__main__":
-#     main()
->>>>>>> 0e6790e96128889d27059087eab0295d137add22
