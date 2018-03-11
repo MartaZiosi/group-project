e6bb8c34338771271355dbe1b8c315826f74dd45
Hiranmayee Rajan <H.Rajan@lse.ac.uk>
2018-03-11 02:03:44 +0000

    Streamlined code, added an option to change password and to sign out

--- website.py
+++ website.py
@@ -59,25 +59,18 @@ def add_data():
 #This function create a new row of values in the table of complaints
 
 
-@app.route("/login")
-def login_form():
-    return render_template("/login.html")
-#Opens login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session.pop("user", None)
+        login_data = request.form
+        if check_login(login_data["username"], login_data["password"]):
+            session["user"] = login_data["username"]
+            return redirect(url_for("homepage"))
+        else:
+            return render_template("wrong_login.html")
+    return render_template("login.html")
 
-
-@app.route("/flat", methods = ["POST"])
-def login_true():
-    login_data = request.form
-    check_login(login_data["username"], login_data["password"])
-    # user_info = check_login(login_data["username"], login_data["password"])
-    if session["logged_in"] == False:
-        return redirect(url_for("login_false"))
-    elif session["user"] == "admin":
-        return redirect(url_for("admin_page"))
-    else:
-        user_complaints = show_selected_complaints(session["user"])
-        return render_template ("/flat_homepage.html", data = user_complaints)
-#Opens flat homepage showing all registered complaints by the user if login successful or redirects to login_false function if login unsuccessful
 
 def check_login(username, password):
     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
@@ -87,27 +80,23 @@ def check_login(username, password):
     username_records = cursor.fetchall()
     for row in username_records:
         if row[0] == username and row[1] == password:
-            session["logged_in"] = True
-            session["user"] = row[0]
             return True
         else:
             continue
-    session["logged_in"] = False
-    session["user"] = None
     return False
-#This function checks if user entered correct login information
 
 
-@app.route("/session")
-def login_false():
-    return render_template("/wrong_login.html")
-#Opens the login unsuccessful page giving users the chance to try again
 
-
-@app.route("/admin")
-def admin_page():
-    records = show_selected_complaints('admin')
-    return render_template("/admin.html", data = records)
+@app.route("/user")
+def homepage():
+    if "user" in session:
+        if session["user"] == "admin":
+            all_complaints = show_selected_complaints('admin')
+            return render_template("admin.html", data = all_complaints)
+        else:
+            user_complaints = show_selected_complaints(session["user"])
+            return render_template ("flat_homepage.html", data = user_complaints)
+    return redirect(url_for("login"))
 
 
 def show_selected_complaints(username):
@@ -130,7 +119,7 @@ def show_selected_complaints(username):
 
 
 
-@app.route("/admin/confirming-submission", methods=["POST"])
+@app.route("/user/confirming-submission", methods=["POST"])
 def update_complaints():
     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
     conn = psycopg2.connect(conn_string)
@@ -147,7 +136,105 @@ def update_complaints():
             cursor.execute("UPDATE complaints SET resolved = False WHERE id = '{}'".format(row[0]))
             conn.commit()
     conn.close()
-    return redirect("/admin")
+    return redirect("/user")
+
+
+
+@app.route("/signout")
+def sign_out():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+# @app.route("/login")
+# def login_form():
+#     return render_template("/login.html")
+# #Opens login page
+#
+#
+# @app.route("/flat", methods = ["POST"])
+# def login_true():
+#     login_data = request.form
+#     check_login(login_data["username"], login_data["password"])
+#     # user_info = check_login(login_data["username"], login_data["password"])
+#     if session["logged_in"] == False:
+#         return redirect(url_for("login_false"))
+#     elif session["user"] == "admin":
+#         return redirect(url_for("admin_page"))
+#     else:
+#         user_complaints = show_selected_complaints(session["user"])
+#         return render_template ("/flat_homepage.html", data = user_complaints)
+# #Opens flat homepage showing all registered complaints by the user if login successful or redirects to login_false function if login unsuccessful
+#
+# def check_login(username, password):
+#     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
+#     conn = psycopg2.connect(conn_string)
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM users")
+#     username_records = cursor.fetchall()
+#     for row in username_records:
+#         if row[0] == username and row[1] == password:
+#             session["logged_in"] = True
+#             session["user"] = row[0]
+#             return True
+#         else:
+#             continue
+#     session["logged_in"] = False
+#     session["user"] = None
+#     return False
+# #This function checks if user entered correct login information
+#
+#
+# @app.route("/session")
+# def login_false():
+#     return render_template("/wrong_login.html")
+# #Opens the login unsuccessful page giving users the chance to try again
+#
+#
+# @app.route("/admin")
+# def admin_page():
+#     records = show_selected_complaints('admin')
+#     return render_template("/admin.html", data = records)
+#
+#
+# def show_selected_complaints(username):
+#     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
+#     conn = psycopg2.connect(conn_string)
+#     cursor = conn.cursor()
+#     if username == "admin":
+#         cursor.execute("SELECT * FROM complaints ORDER BY created DESC")
+#         records = cursor.fetchall()
+#     else:
+#         cursor.execute("SELECT * FROM complaints WHERE flat = '{}' ORDER BY created DESC".format(username))
+#         records = cursor.fetchall()
+#
+#     for row in records:
+#         i = records.index(row)
+#         dmyt_format_time = row[3].strftime('%b %d, %Y at %H:%M')
+#         row = row[:3] + (dmyt_format_time, ) + row[4:]
+#         records[i] = row
+#     return records
+#
+#
+#
+# @app.route("/admin/confirming-submission", methods=["POST"])
+# def update_complaints():
+#     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
+#     conn = psycopg2.connect(conn_string)
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT id FROM complaints")
+#     records = cursor.fetchall()
+#     for row in records:
+#         checkbox_name = "resolved_check_comment_{}".format(row[0])
+#         if request.form.get(checkbox_name):
+#             # return "Updated {}".format(checkbox_name)
+#             cursor.execute("UPDATE complaints SET resolved = True WHERE id = '{}'".format(row[0]))
+#             conn.commit()
+#         else:
+#             cursor.execute("UPDATE complaints SET resolved = False WHERE id = '{}'".format(row[0]))
+#             conn.commit()
+#     conn.close()
+#     return redirect("/admin")
 
 
 
