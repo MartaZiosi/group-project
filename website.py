75c7b7104d1c2cf4a387796e80ec9e9b38437af4
Hiranmayee Rajan <H.Rajan@lse.ac.uk>
2018-03-13 02:29:41 +0000

    Added option to change password, and basic design elements including navbar

--- website.py
+++ website.py
@@ -15,6 +15,29 @@ def complaints_form():
 #Opens to the home page conatining links to all other pages
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+@app.route("/announcements")
+def announcement():
+    conn_string = "host = 'ec2-184-73-196-65.compute-1.amazonaws.com' dbname = 'd1o7vtp5j9d3ir' user='xsxjgcoihanbjr' password='d5b16cb44ef8e47998cc4c1bbd25f73a755986d01bda1875fa5f38f10aaf9d23'"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM announcements ORDER BY posted DESC")
+    records = cursor.fetchall()
+    conn.close()
+    for row in records:
+        i = records.index(row)
+        dmyt_format_time = row[2].strftime('%b %d, %Y at %H:%M')
+        row = row[:2] + (dmyt_format_time, )
+        records[i] = row
+    return render_template("announcements.html", records_list = records)
+
+
+
 @app.route("/complaints", methods=["GET"])
 def show_complaints():
     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
@@ -54,7 +77,7 @@ def add_data():
     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO complaints (flat, name, comment, created, resolved) VALUES ('{}','{}', '{}', '{}', '{}')".format(form_data["flat"], form_data["name"], form_data["comment"], str(datetime.now()), False))
+    cursor.execute("INSERT INTO complaints (flat, name, comment, created, resolved) VALUES ('{}','{}', '{}', '{}', '{}')".format(session["user"], form_data["name"], form_data["comment"], str(datetime.now()), False))
     conn.commit()
     conn.close()
 #This function create a new row of values in the table of complaints
@@ -110,6 +133,7 @@ def show_selected_complaints(username):
     else:
         cursor.execute("SELECT * FROM complaints WHERE flat = '{}' ORDER BY created DESC".format(username))
         records = cursor.fetchall()
+    conn.close()
 
     for row in records:
         i = records.index(row)
@@ -147,96 +171,38 @@ def sign_out():
     return redirect(url_for("login"))
 
 
-# @app.route("/login")
-# def login_form():
-#     return render_template("/login.html")
-# #Opens login page
-#
-#
-# @app.route("/flat", methods = ["POST"])
-# def login_true():
-#     login_data = request.form
-#     check_login(login_data["username"], login_data["password"])
-#     # user_info = check_login(login_data["username"], login_data["password"])
-#     if session["logged_in"] == False:
-#         return redirect(url_for("login_false"))
-#     elif session["user"] == "admin":
-#         return redirect(url_for("admin_page"))
-#     else:
-#         user_complaints = show_selected_complaints(session["user"])
-#         return render_template ("/flat_homepage.html", data = user_complaints)
-# #Opens flat homepage showing all registered complaints by the user if login successful or redirects to login_false function if login unsuccessful
-#
-# def check_login(username, password):
-#     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
-#     conn = psycopg2.connect(conn_string)
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT * FROM users")
-#     username_records = cursor.fetchall()
-#     for row in username_records:
-#         if row[0] == username and row[1] == password:
-#             session["logged_in"] = True
-#             session["user"] = row[0]
-#             return True
-#         else:
-#             continue
-#     session["logged_in"] = False
-#     session["user"] = None
-#     return False
-# #This function checks if user entered correct login information
-#
-#
-# @app.route("/session")
-# def login_false():
-#     return render_template("/wrong_login.html")
-# #Opens the login unsuccessful page giving users the chance to try again
-#
-#
-# @app.route("/admin")
-# def admin_page():
-#     records = show_selected_complaints('admin')
-#     return render_template("/admin.html", data = records)
-#
-#
-# def show_selected_complaints(username):
-#     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
-#     conn = psycopg2.connect(conn_string)
-#     cursor = conn.cursor()
-#     if username == "admin":
-#         cursor.execute("SELECT * FROM complaints ORDER BY created DESC")
-#         records = cursor.fetchall()
-#     else:
-#         cursor.execute("SELECT * FROM complaints WHERE flat = '{}' ORDER BY created DESC".format(username))
-#         records = cursor.fetchall()
-#
-#     for row in records:
-#         i = records.index(row)
-#         dmyt_format_time = row[3].strftime('%b %d, %Y at %H:%M')
-#         row = row[:3] + (dmyt_format_time, ) + row[4:]
-#         records[i] = row
-#     return records
-#
-#
-#
-# @app.route("/admin/confirming-submission", methods=["POST"])
-# def update_complaints():
-#     conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
-#     conn = psycopg2.connect(conn_string)
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT id FROM complaints")
-#     records = cursor.fetchall()
-#     for row in records:
-#         checkbox_name = "resolved_check_comment_{}".format(row[0])
-#         if request.form.get(checkbox_name):
-#             # return "Updated {}".format(checkbox_name)
-#             cursor.execute("UPDATE complaints SET resolved = True WHERE id = '{}'".format(row[0]))
-#             conn.commit()
-#         else:
-#             cursor.execute("UPDATE complaints SET resolved = False WHERE id = '{}'".format(row[0]))
-#             conn.commit()
-#     conn.close()
-#     return redirect("/admin")
 
+@app.route("/user/account-info")
+def account_info():
+    return render_template("account_info.html")
+
+
+
+@app.route("/user/change-account-info", methods = ["POST"])
+def change_info():
+    records = request.form
+    if change_password(records["old-password"], records["new-password"]):
+        session.pop("user", None)
+        return render_template("successful_change.html")
+    else:
+        return render_template("unsuccessful_change.html")
+
+
+def change_password(old_password, new_password):
+    conn_string = "host = 'host_name' dbname = 'database_name' user='username' password='password'"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    # username = session["user"]
+    cursor.execute("SELECT password FROM users WHERE username = '{}'".format(session["user"]))
+    records = cursor.fetchall()
+    if records[0][0] == old_password:
+        cursor.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(new_password, session["user"]))
+        conn.commit()
+        conn.close()
+        return True
+    else:
+        conn.close()
+        return False
 
 
 app.run(debug = True)
